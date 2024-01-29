10bba9f802dac087c7650aaa68fefd34cfeff39c
stappost <emanuele.scarpantoni@gmail.com>
2024-01-29 14:45:09 +0100

    add vue, function get list in js. get list in server

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            apiUrl: 'server.php',
+            toDoList: [],
+        }
+    },
+    mounted() {
+        this.getTodoList()
+    },
+    methods: {
+        getTodoList() {
+            axios.get(this.apiUrl).then((response) => {
+                this.toDoList = response.data;
+                console.log(response.data)
+            })
+        }
+    }
+}).mount('#app')
---
+++ server.php
@@ -0,0 +1,5 @@
+<?php 
+    $list = file_get_contents('toDoList.json');
+
+    echo $list
+?>
